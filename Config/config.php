d08f80ec3fd549330fb36fe5027331b2c8b89ba6
sagsoz06 <sagsoz06@gmail.com>
2017-03-31 17:47:24 +0300

    Block Module

---
+++ Config/config.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Partials to include on page views
+    |--------------------------------------------------------------------------
+    | List the partials you wish to include on the different type page views
+    | The content of those fields well be caught by the PageWasCreated and PageWasEdited events
+    */
+    'partials' => [
+        'translatable' => [
+            'create' => [],
+            'edit' => [],
+        ],
+        'normal' => [
+            'create' => [],
+            'edit' => [],
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Dynamic relations
+    |--------------------------------------------------------------------------
+    | Add relations that will be dynamically added to the Block entity
+    */
+    'relations' => [
+//        'extension' => function ($self) {
+//            return $self->belongsTo(BlockExtension::class, 'id', 'block_id')->first();
+//        }
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Sidebar Class
+    |--------------------------------------------------------------------------
+    | If you want to customise the admin sidebar ordering or grouping
+    | You can define your own sidebar class for this module.
+    | No custom sidebar: null
+    */
+    'custom-sidebar' => null,
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Load additional view namespaces for a module
+    |--------------------------------------------------------------------------
+    | You can specify place from which you would like to use module views.
+    | You can use any combination, but generally it's advisable to add only one,
+    | extra view namespace.
+    | By default every extra namespace will be set to false.
+    */
+    'useViewNamespaces' => [
+        // Read module views from /Themes/<backend-theme-name>/views/modules/<module-name>
+        'backend-theme' => false,
+        // Read module views from /Themes/<frontend-theme-name>/views/modules/<module-name>
+        'frontend-theme' => false,
+        // Read module views from /resources/views/<module-name>
+        'resources' => true,
+    ],
+];
---
+++ Providers/BlockServiceProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Modules\Block\Providers;
+
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\ServiceProvider;
+use Modules\Block\Entities\Block;
+use Modules\Block\Facades\BlockFacade;
+use Modules\Block\Repositories\Cache\CacheBlockDecorator;
+use Modules\Block\Repositories\Eloquent\EloquentBlockRepository;
+use Modules\Core\Traits\CanPublishConfiguration;
+
+class BlockServiceProvider extends ServiceProvider
+{
+    use CanPublishConfiguration;
+    /**
+     * Indicates if loading of the provider is deferred.
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerBindings();
+        $this->registerFacade();
+    }
+
+    public function boot()
+    {
+        $this->publishConfig('block', 'permissions');
+        $this->publishConfig('block', 'config');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+    private function registerBindings()
+    {
+        $this->app->bind(
+            'Modules\Block\Repositories\BlockRepository',
+            function () {
+                $repository = new EloquentBlockRepository(new Block());
+
+                if (! config('app.cache')) {
+                    return $repository;
+                }
+
+                return new CacheBlockDecorator($repository);
+            }
+        );
+    }
+
+    private function registerFacade()
+    {
+        $aliasLoader = AliasLoader::getInstance();
+        $aliasLoader->alias('Block', BlockFacade::class);
+    }
+}
